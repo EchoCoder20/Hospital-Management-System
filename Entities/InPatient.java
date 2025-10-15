fe99a64bfea462333777d94657cfd6d58beaf21f
EchoCoder20 <albusaidi1915@gmail.com>
2025-10-15 14:45:11 +0400

    Adding Patient Extend Classes

---
+++ Entities/InPatient.java
@@ -0,0 +1,77 @@
+package Entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class InPatient extends Patient {
+    private LocalDate admissionDate;
+    private LocalDate dischargeDate;
+    private String roomNumber;
+    private String bedNumber;
+    private String admittingDoctorId;
+    private double dailyCharges;
+
+    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String patientId, String bloodGroup, List<String> allergies, String emergencyContact, LocalDate registrationDate, String insuranceId, List<MedicalRecord> medicalRecords, List<Appointment> appointments, LocalDate admissionDate, LocalDate dischargeDate, String roomNumber, String bedNumber, String admittingDoctorId, double dailyCharges) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, patientId, bloodGroup, allergies, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments);
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber = roomNumber;
+        this.bedNumber = bedNumber;
+        this.admittingDoctorId = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
+    }
+
+    public LocalDate getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(LocalDate admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public LocalDate getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(LocalDate dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getBedNumber() {
+        return bedNumber;
+    }
+
+    public void setBedNumber(String bedNumber) {
+        this.bedNumber = bedNumber;
+    }
+
+    public String getAdmittingDoctorId() {
+        return admittingDoctorId;
+    }
+
+    public void setAdmittingDoctorId(String admittingDoctorId) {
+        this.admittingDoctorId = admittingDoctorId;
+    }
+
+    public double getDailyCharges() {
+        return dailyCharges;
+    }
+
+    public void setDailyCharges(double dailyCharges) {
+        this.dailyCharges = dailyCharges;
+    }
+    public void  calculateStayDuration(){
+
+    }
+    public void calculateTotalCharges(){
+
+    }
+}
---
+++ Entities/OutPatient.java
@@ -0,0 +1,47 @@
+package Entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class OutPatient extends Patient {
+    private int visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
+
+    public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender, String phoneNumber, String email, String address, String patientId, String bloodGroup, List<String> allergies, String emergencyContact, LocalDate registrationDate, String insuranceId, List<MedicalRecord> medicalRecords, List<Appointment> appointments, int visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, patientId, bloodGroup, allergies, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments);
+        this.visitCount = visitCount;
+        this.lastVisitDate = lastVisitDate;
+        this.preferredDoctorId = preferredDoctorId;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+
+    public void setVisitCount(int visitCount) {
+        this.visitCount = visitCount;
+    }
+
+    public LocalDate getLastVisitDate() {
+        return lastVisitDate;
+    }
+
+    public void setLastVisitDate(LocalDate lastVisitDate) {
+        this.lastVisitDate = lastVisitDate;
+    }
+
+    public String getPreferredDoctorId() {
+        return preferredDoctorId;
+    }
+
+    public void setPreferredDoctorId(String preferredDoctorId) {
+        this.preferredDoctorId = preferredDoctorId;
+    }
+    public void scheduleFollowUp(){
+
+    }
+    public void updateVisitCount(){
+
+    }
+}
